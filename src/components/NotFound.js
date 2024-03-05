a7ddcd15cbfde4f29f6c17b3511c873745ccaa10
piyushdobariya3255 <piyushdobariya025@gmail.com>
2024-03-05 08:49:54 +0530

    not found ready

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -4,6 +4,8 @@ import { NavLink } from 'react-router-dom'
 
 function NotFound() {
   return (
+
+    // ready for NotFound
     <div className='notfound'>
        <div className="container">
       <section>
